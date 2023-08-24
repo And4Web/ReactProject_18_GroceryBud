07d950168ad371ec64f7836fa3f392e156f43be1
unknown <anands2807@gmail.com>
2023-08-24 15:32:21 +0530

    App.js, Alert.js and List.js 3 components created

---
+++ src/List.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {FaEdit, FaTrash} from 'react-icons/fa'
+
+function List() {
+  return (
+    <div>
+      <h1>List Component</h1>
+      <div>
+        <FaEdit/>
+        <FaTrash/>
+      </div>
+    </div>
+  )
+}
+
+
+export default List
